918d0f554d50b5eee9504e077c0d579975c944fa
Euller Cristian <eullercdr@gmail.com>
2020-02-29 12:29:31 -0300

    Resetando a senha utilizando o token

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -31,7 +32,27 @@ class ForgotPasswordController {
   }
 
   async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+      const user = await User.findByOrFail('token', token)
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'Token Expirado' } })
+      }
 
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+      await user.save()
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Algo deu errado' } })
+    }
   }
 }
 
